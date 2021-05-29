074ca58996bd839dbf04240ebff9c443d362fd08
32Moon <lmj970302@gmail.com>
2021-05-30 01:20:34 +0900

    ✨현재 위치 주소로 나타내기

--- AppleMap/ViewController.swift
+++ AppleMap/ViewController.swift
@@ -29,6 +29,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         myMap.showsUserLocation = true
     }
+    // 위도, 경도로 위치 표시
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        
+        myMap.setRegion(pRegion, animated: true)
+    }
+    // 위치가 업데이트 되었을 때 지도에 위치 표시
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pLocation = locations.last
+        goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        // 위도와 경도의 값으로 주소 찾기
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
+            (placemark, erro) -> Void in
+            let pm = placemark!.first
+            let country = pm!.country
+            var address: String = country!
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.locality!
+            }
+            if pm!.thoroughfare != nil {
+                address += " "
+                address += pm!.thoroughfare!
+            }
+            self.locationInfoLabel.text = "현재 위치"
+            self.locationLabel.text = address
+        })
+        locationManager.stopUpdatingHeading()
+    }
     
     @IBAction func changeLocationSg(_ sender: UISegmentedControl) {
     }
